0a436b2f4e10cade71aae9c74a874054c97a10ef
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-10-19 09:33:29 +0200

    feat: add db

---
+++ components/layout/PublicLayout.jsx
@@ -0,0 +1,24 @@
+import { Box, ChakraProvider} from "@chakra-ui/react";
+import Head from "next/head";
+// import { ReactNode } from "react";
+import Navbar from "../NavBar";
+import theme from "../../utils/chakra-theme";
+
+
+export default function PublicLayout({ children }) {
+  return (
+    <Box minH={"100vh"} bg={"#1A171E"}>
+      <Head>
+        <title>YnovPhantomX UTILS</title>
+        <meta name="description" content="YnovPhantomX database utils ! Explore many utils for pentest or hacking !" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:image" content="/logo.png" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Navbar/>
+        <main>{children}</main>
+      </ChakraProvider>
+    </Box>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import ContainerBlock from '@/components/ContainerBlock'
 export default function Home() {
   return (
     <ContainerBlock>
-
+      hello
     </ContainerBlock>
   )
 }
